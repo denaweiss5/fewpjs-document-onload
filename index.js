af27adb7c75b2c0e74c81c8b18d349cb7fb1c7ae
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-10 11:30:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(updateDOM());
+  }); 
+
+  function updateDOM() {
+    const text = document.getElementById('text').textContent = 'This is really cool!';
+  }
